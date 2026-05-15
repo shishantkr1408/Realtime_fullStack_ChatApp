7cd0ca44ed5a3d8679f26f59f8af9796b24a9851
shishantkr1408 <shishantkr2003@gmail.com>
2026-05-15 20:28:05 +0530

    fixed some errors in package.json and cors for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,9 @@ const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.NODE_ENV === "production"
+  ? "https://your-render-url.onrender.com"
+  : "http://localhost:5173",
     credentials:true
 }
 ))
